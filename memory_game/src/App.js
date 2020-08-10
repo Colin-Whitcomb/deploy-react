9b9697dec427ad65f6c2fd47adb8061abc27b0de
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-09 17:47:21 -0700

    [add] multiple cards.

--- memory_game/src/App.js
+++ memory_game/src/App.js
@@ -1,18 +1,38 @@
 import React from "react";
 import CharacterCard from './components/CharacterCard/index';
-// import Wrapper from './src/components/Wrapper';
+import Wrapper from './components/Wrapper/index';
 import characters from './characters.json';
 
 function App () {
     return (
-        <div>
+        <Wrapper>
             <h2 className="title">React Memory Game!</h2>
             <CharacterCard 
                 name={characters[0].name}
                 image={characters[0].image}
                 id={characters[0].id}
             />
-        </div>
+              <CharacterCard 
+                name={characters[1].name}
+                image={characters[1].image}
+                id={characters[1].id}
+            />
+             <CharacterCard 
+                name={characters[2].name}
+                image={characters[2].image}
+                id={characters[2].id}
+            />
+             <CharacterCard 
+                name={characters[3].name}
+                image={characters[3].image}
+                id={characters[3].id}
+            />
+             <CharacterCard 
+                name={characters[4].name}
+                image={characters[4].image}
+                id={characters[4].id}
+            />
+        </Wrapper>
     );
 }
 
