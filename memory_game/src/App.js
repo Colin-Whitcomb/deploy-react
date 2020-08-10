b5e1a979af17ed954d7ee5ccce93df640e7f188a
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-10 16:41:15 -0700

    [add] character maped onto character cards.

--- memory_game/src/App.js
+++ memory_game/src/App.js
@@ -1,43 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import CharacterCard from './components/CharacterCard/index';
 import Wrapper from './components/Wrapper/index';
 import characters from './characters.json';
 import Jumbotron from "./components/Jumbotron/jumbo";
 import Navbar from "./components/Navbar/navbar"
 
-function App () {
+
+class App extends Component {
+
+    state = {
+        characters: characters
+    };
+
+render () {
     return (
         <Wrapper>
             <Navbar />
             <Jumbotron />
             <h2 className="title">React Memory Game!</h2>
-            <CharacterCard 
-                name={characters[0].name}
-                image={characters[0].image}
-                id={characters[0].id}
+            {this.state.characters.map(character => (
+                <CharacterCard 
+                name={character.name}
+                image={character.image}
+                id={character.idid}
             />
-              <CharacterCard 
-                name={characters[1].name}
-                image={characters[1].image}
-                id={characters[1].id}
-            />
-             <CharacterCard 
-                name={characters[2].name}
-                image={characters[2].image}
-                id={characters[2].id}
-            />
-             <CharacterCard 
-                name={characters[3].name}
-                image={characters[3].image}
-                id={characters[3].id}
-            />
-             <CharacterCard 
-                name={characters[4].name}
-                image={characters[4].image}
-                id={characters[4].id}
-            />
+            ))}
+           
+             
         </Wrapper>
     );
 }
-
+}
 export default App;
