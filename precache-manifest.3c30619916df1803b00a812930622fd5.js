ee1f8c6035210b7222a0303b8abf5dd897beb851
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-11 20:50:23 -0700

    Updates

---
+++ precache-manifest.3c30619916df1803b00a812930622fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debcd6e4541b4f78fbe4f8a51d8f3b9e",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "3d48fee9e815b2955d20",
+    "url": "/deploy-react/static/css/main.3796eb63.chunk.css"
+  },
+  {
+    "revision": "4cb0ff2b6213b3374c6c",
+    "url": "/deploy-react/static/js/2.9a2ca08a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.9a2ca08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d48fee9e815b2955d20",
+    "url": "/deploy-react/static/js/main.d011db16.chunk.js"
+  },
+  {
+    "revision": "a13f478a9ccf9004c07d",
+    "url": "/deploy-react/static/js/runtime-main.a55e94bc.js"
+  }
+]);
